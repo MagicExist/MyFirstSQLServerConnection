e30f160c30d19a163532db9a275b9cfb75402c20
Johhan <Johhanparra1@gmail.com>
2023-09-20 17:47:41 -0500

    Delete static get method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,45 +4,6 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-
-        public static List<Personas> Get()
-        {
-
-            string connectionString = @"Data Source=JOHHAN\SQLEXPRESS;Initial Catalog=PERSONAS;User=sa;Password=12345678;TrustServerCertificate = True";
-
-
-            List<Personas> list = new List<Personas>();
-
-            string query = "SELECT ID,NOMBRE,APELLIDO,CARRERA FROM ESTUDIANTES";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand cmd = new SqlCommand(query, connection);
-                try
-                {
-                    connection.Open();
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Personas oPersona = new Personas();
-                        oPersona.Id = reader.GetInt32(0);
-                        oPersona.Nombre = reader.GetString(1);
-                        oPersona.Apellido = reader.GetString(2);
-                        oPersona.Carrera = reader.GetString(3);
-                        list.Add(oPersona);
-                    }
-                    reader.Close();
-                    connection.Close();
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-            }
-
-
-            return list;
-        }
         public static void Add(string Nombre, string Apellido, string Carrera)
         {
 
